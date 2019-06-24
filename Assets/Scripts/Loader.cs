0d26a90911ba8234d37dced10dfeedea40b8004a
Wu, Bin (Daniel) <binwu@ea.com>
2019-06-24 16:59:13 +0800

    initial commit

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    public GameManager gameManager;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(GameManager.instance == null)
+        {
+            Instantiate(gameManager);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
