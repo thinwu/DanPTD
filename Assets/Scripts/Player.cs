c281a0418aaadbeb67b890f1d63a121fd3cda048
Wu, Bin (Daniel) <binwu@ea.com>
2019-06-26 18:05:30 +0800

    adding dailywork

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MovingObject
+{
+    public int wallDamage = 1;
+    public int pointsPerFood = 10;
+    public int pointsPerSoda = 20;
+    public float restartLevelDelay = 1f;
+    public Text foodText;
+
+    private Animator animator;
+    private int food;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+        food = GameManager.instance.playerFoodPoints;
+        foodText.text = "Food: " + food;
+        base.Start();
+    }
+    //This function is called when the behaviour becomes disabled or inactive.
+    private void OnDisable()
+    {
+        //When Player object is disabled, store the current local food total in the GameManager so it can be re-loaded in next level.
+        GameManager.instance.playerFoodPoints = food;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameManager.instance.playersTurn) return;
+        
+        int horizontal = 0;
+        int vertical = 0;
+
+        horizontal = (int)(Input.GetAxisRaw("Horizontal"));
+        vertical = (int)(Input.GetAxisRaw("Vertical"));
+
+        if (horizontal != 0)
+        {
+            vertical = 0;
+        }
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            AttemptMove<Wall>(horizontal, vertical);
+        }
+    }
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        food--;
+        base.AttemptMove<T>(xDir, yDir);
+        foodText.text = "Food: " + food;
+        RaycastHit2D hit;
+
+        if (Move(xDir, yDir, out hit))
+        {
+
+        }
+
+        CheckIfGameOver();
+        
+        GameManager.instance.playersTurn = false;
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Wall hitWall = component as Wall;
+        hitWall.DamageWall(wallDamage);
+        animator.SetTrigger("playerChop");
+    }
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        switch (other.tag)
+        {
+            case "Exit":
+                Invoke("Restart", restartLevelDelay);
+                enabled = false;
+                break;
+            case "Food":
+                food += pointsPerFood;
+                foodText.text = "+" + pointsPerFood + " Food: " + food;
+                other.gameObject.SetActive(false);
+                break;
+            case "Soda":
+                food += pointsPerSoda;
+                foodText.text = "+" + pointsPerSoda + " Food: " + food;
+                other.gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoseFood(int loss)
+    {
+        animator.SetTrigger("playerHit");
+
+        food -= loss;
+        foodText.text = "-" + loss + " Food: " + food;
+        CheckIfGameOver();
+    }
+
+    private void CheckIfGameOver()
+    {
+        if(food <= 0)
+        {
+            GameManager.instance.GameOver();
+        }
+    }
+}
